d4988aedd4a057935988832a0183ba827fa4996c
Taryn Shuler <tarynashleyshuler@gmail.com>
2015-12-05 08:13:46 -0800

    finish compare foods

--- file-exercises/compare_favs.py
+++ file-exercises/compare_favs.py
@@ -7,4 +7,19 @@ patrick_favs = f2.readlines()
 f1.close()
 f2.close()
 
+#def compare_favs(fav1, fav2):
+#	if fav1 == fav2:
+#		return "Our favorite foods are the same!"
+#	else:
+#		return "our favorite foods are different!"
+
+#print compare_favs(taryn_favs,patrick_favs)
+
+def compare_favs2(fav1, fav2):
+	for fav in fav1:
+		if fav in fav2:
+			print "We both love %s" %fav.strip('/n')
+
+compare_favs2(taryn_favs,patrick_favs)
+
 
